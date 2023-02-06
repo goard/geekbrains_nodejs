74b569e852d9a930e3be5a9b75d3daf6f1868a9a
Ayur <ayur.gonchikov@teamlead.ru>
2023-02-06 12:20:48 +0800

    lesson5

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
 const http = require('http')
 const path = require('path')
 const fsp = require('fs/promises')
-const inquirer = require('inquirer')
+const fs = require('fs')
 
 const host = 'localhost'
 const port = 3000
 const resDir = __dirname
 
-let pathVar = ''
+let inPath = resDir
+let link = ''
+
+async function checkIsDir(inPath) {
+  const stat = await fsp.stat(inPath)
+  return stat.isDirectory()
+}
 
 function readDirInquirerChoose(inPath) {
-  if (pathVar !== '') {
-    pathVar = path.join(pathVar, inPath)
-  } else {
-    pathVar = inPath
-  }
-  return new Promise((resolve, reject) => {
-    fsp
-      .readdir(pathVar)
-      .then((choices) => {
-        return inquirer.prompt({
-          name: 'fileName',
-          type: 'list', // input, number, confirm, list, rawlist, expand, checkbox, password
-          message: 'Choose file',
-          choices,
-        })
-      })
-      .then(async ({ fileName }) => {
-        const stat = await fsp.stat(path.join(pathVar, fileName))
-        if (stat.isDirectory()) {
-          return await readDirInquirerChoose(fileName)
-        }
-        return fileName
-      })
-      .then(resolve)
-      .catch(reject)
+  return new Promise(async (resolve, reject) => {
+    await fsp.readdir(inPath).then(resolve).catch(reject)
   })
 }
+
 const server = http.createServer(async (req, res) => {
-  let data =null
-  data  = await readDirInquirerChoose(resDir)
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
 
-  if(data) {
-    res.end(JSON.stringify(data))
+  if (req.url != '/favicon.ico') {
+    const url = req.url.substring(1, req.url.length)
+
+    if (url !== '') {
+      inPath += `/${url}`
+      link += `/${url}`
+      const isDirectory = await checkIsDir(inPath)
+      if (isDirectory) {
+        readDirInquirerChoose(inPath).then((data) => {
+          arr = data.map((el) => `<li><a href="${el}">${el}</a></li>`).join(' ')
+          res.end(arr)
+        })
+      } else {
+        const readStream = fs.createReadStream(inPath, { encoding: 'utf-8' })
+
+        readStream.on('data', (chunk) => {
+          res.write(chunk)
+        })
+
+        readStream.on('end', () => {
+          res.end()
+        })
+      }
+    } else {
+      inPath = resDir
+      readDirInquirerChoose(resDir).then((data) => {
+        arr = data.map((el) => `<li><a href="${el}">${el}</a></li>`).join(' ')
+        res.end(arr)
+      })
+    }
   }
-  res.end(JSON.stringify(resDir))
 })
-
 
 server.listen(port, host, () =>
   console.log(`Server run at http://${host}:${port}`)
